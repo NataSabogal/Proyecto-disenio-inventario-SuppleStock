a57ea09d446244aa4fe5a2c77bb391432208b8c4
Brandon Zapata <zapata.brandon.9193.@eam.edu.co>
2025-05-03 10:57:51 -0500

    Agregado VentanaSuplementos y modificado ProyectoSuppleStock y pom.xml

--- src/main/java/com/mycompany/proyectosupplestock/ProyectoSuppleStock.java
+++ src/main/java/com/mycompany/proyectosupplestock/ProyectoSuppleStock.java
@@ -8,9 +8,9 @@ package com.mycompany.proyectosupplestock;
  *
  * @author nataliasabogalrada
  */
-public class ProyectoSuppleStock {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+//public class ProyectoSuppleStock {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
